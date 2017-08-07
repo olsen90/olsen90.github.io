8cbb88f608d349c78f7d2cc0a2f60209264e393b
Anders Olsen <andersolsen1507@gmail.com>
2017-08-07 23:23:23 +0200

    Updating the website

---
+++ main.js
@@ -0,0 +1,13 @@
+// This is a comment.
+
+var name = 'Anders';
+//
+var age = 27;
+
+function introduction(name,age){
+  alert('The owner of this webpage is called '+name+', and he is '+age+' years old.')
+}
+
+var age = prompt('What is your age?');
+
+introduction(name,age);
